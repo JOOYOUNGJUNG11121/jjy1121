dd9f7753c61997cfbec644afee3b6b72ff1ef24f
JOOYOUNGJUNG11121 <jjy20051121jung@gmail.com>
2025-05-28 15:01:31 +0900

    Add files via upload

---
+++ python tast.py
@@ -0,0 +1,60 @@
+#덧셈
+print (1 + 2)
+#뺄셈
+print (2 - 1) 
+#곱셈
+print (2 * 3)
+#나눗셈
+print (6 / 2)
+#나머지
+print (7 % 3)
+#거듭제곱
+print (2 ** 3)
+# 몫
+print (7 // 3)
+# 변수 선언
+a = 10
+b = 5
+# 변수 덧셈
+print(a + b)
+# 변수 뺄셈
+print(a - b)
+# 변수 곱셈
+print(a * b)
+# 변수 나눗셈
+print(a / b)
+# 변수 나머지
+print(a % b)
+# 변수 거듭제곱
+print(a ** b)
+# 변수 몫
+print(a // b)
+# 변수 선언
+x = 15
+y = 4
+# 변수 덧셈
+print(x + y)
+# 변수 뺄셈
+print(x - y)
+# 변수 곱셈
+print(x * y)
+# 변수 나눗셈
+print(x / y)
+# 변수 나머지
+print(x % y)
+# 변수 거듭제곱
+print(x ** y)
+# 변수 몫
+print(x // y)
+
+print (divmod(3, 1))
+print (divmod(15, 2))
+print (divmod(17, 6))
+print (divmod(13, 3))
+print (divmod(16, 7))
+print (divmod(17, 4))
+print (divmod(19, 2))
+
+
+print(7//3,"." , 6%3)
+
